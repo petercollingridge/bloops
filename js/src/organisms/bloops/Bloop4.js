a7c49efdf8c1fd9d8960a77174f5c90bf9ff1d01
Peter Collingridge <peter.collingridge@nanoporetech.com>
2021-02-20 16:31:30 +0000

    Get Bloop4 phenotype working

--- js/src/organisms/bloops/Bloop4.js
+++ js/src/organisms/bloops/Bloop4.js
@@ -12,17 +12,20 @@ Bloop4.prototype = Object.create(Bloop.prototype);
 
 Bloop4.prototype.calculatePhenotype = function() {
     this.r = Math.sqrt(this.genome[0]);
-    this.metabolism = this.genome[1] * 0.02 + 0.02;
-    this.speed = (101 - this.genome[0]) * (this.metabolism - 0.02) * 0.005;
-    this.reproductionThreshold = 200 + this.genome[2] * 25;
+    const energyForSpeed = this.genome[1] * 0.02;
+    this.metabolism = energyForSpeed + 0.02;
+    this.speed = (101 - this.genome[0]) * energyForSpeed * 0.005;
+    // this.reproductionThreshold = 200 + this.genome[2] * 25;
     this.angle = Math.PI * Math.random();
 };
 
 Bloop4.prototype.getChildGenome = function() {
-    return this.genome.map(mutate);
+    const childGenome = this.genome.map(gene => mutate(gene));
+    return childGenome;
 };
 
 Bloop4.getRandomGenome = function() {
+    return [50, 50, 50];
     return [
         randomInRange(1, 100),  // Size
         randomInRange(1, 100),  // Metabolism
