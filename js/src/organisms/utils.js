6e58f4a44269d6038f423c15ad4c1dac45155520
Peter Collingridge <peter.collingridge@nanoporetech.com>
2022-09-15 22:45:34 +0100

    Function to create random genomes

--- js/src/organisms/utils.js
+++ js/src/organisms/utils.js
@@ -12,6 +12,20 @@ function randomInRange(a, b) {
     return a + Math.floor(Math.random() * (b - a));
 }
 
+// Get a random array of n genes
+// Each gene is an integer between <min> and <max>
+function getRandomGenome(n, min = 1, max = 100) {
+  const genome = [];
+  while(n-- > 0) {
+    genome.push(randomInRange(min, max));
+  }
+  return genome;
+}
+
+// Given value, within a range of min - max,
+// leave it unchanged (p = 0.5)
+// change its value by +1 or -1 (p = 0.49)
+// or pick a new value at random  (p = 0.01)
 function mutate(value, min = 1, max = 100) {
     const mutation = Math.random();
     if (mutation < 0.01) {
