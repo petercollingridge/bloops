f8cdbfc958a3154d08bdb2aea785db81658479c8
Peter Collingridge <peter.collingridge@nanoporetech.com>
2025-01-26 22:23:56 +0000

    Update sim 3

--- js/src/organisms/bloops/Bloop3.js
+++ js/src/organisms/bloops/Bloop3.js
@@ -5,24 +5,23 @@
 
 // require ./Bloop.js
 
-const Bloop3 = function(position, energy, genome) {
-    Bloop.call(this, position, energy, genome);
-    this.childType = Bloop3;
-};
-Bloop3.prototype = Object.create(Bloop.prototype);
-
-Bloop3.prototype.calculatePhenotype = function() {
-    this.r = Math.sqrt(this.genome);
-    this.speed = (101 - this.genome) * 0.005;
-    this.angle = Math.PI * Math.random();
-};
-
-Bloop3.prototype.getChildGenome = function() {
-    const mutation = Math.random();
-    if (mutation < 0.25 && this.genome < 100) {
-        return this.genome + 1;
-    } else if (mutation < 0.50 && this.genome > 1) {
-        return this.genome - 1;
+class Bloop3 extends Bloop {
+    constructor(position, energy, genome) {
+        super(position, energy, genome);
+        this.childType = Bloop3;
     }
-    return this.genome;
-};
+    calculatePhenotype() {
+        this.r = Math.sqrt(this.genome);
+        this.speed = (101 - this.genome) * 0.005;
+        this.angle = Math.PI * Math.random();
+    }
+    getChildGenome() {
+        const mutation = Math.random();
+        if (mutation < 0.25 && this.genome < 100) {
+            return this.genome + 1;
+        } else if (mutation < 0.50 && this.genome > 1) {
+            return this.genome - 1;
+        }
+        return this.genome;
+    }
+}
