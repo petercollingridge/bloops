028155a1905a4f1db6710273aea8d95027686d62
Peter Collingridge <peter.collingridge@nanoporetech.com>
2025-01-26 22:24:07 +0000

    Update recorder class

--- js/src/interface/Recorder.js
+++ js/src/interface/Recorder.js
@@ -1,58 +1,56 @@
 // Object for recording values about the world and downloading as a tab-delimited file
 // Every interval it saves data about the world into an array which can be downloaded
 
-const Recorder = function (fields, interval, world) {
-    this.fields = fields;
-    this.fieldNames = Object.keys(fields);
-    this.interval = interval;
-    this.world = world;
+class Recorder {
+    constructor(fields, interval, world) {
+        this.fields = fields;
+        this.fieldNames = Object.keys(fields);
+        this.interval = interval;
+        this.world = world;
 
-    // Map field name to an array of data
-    this.data = {};
-    this.fieldNames.forEach(fieldName => {
-        this.data[fieldName] = [];
-    })
-    this.dataLength = 0;
-};
-
-Recorder.prototype.update = function() {
-    if (this.world.time % this.interval === 0) {
-        this.dataLength++;
+        // Map field name to an array of data
+        this.data = {};
         this.fieldNames.forEach(fieldName => {
-            const value = this.fields[fieldName](this.world);
-            this.data[fieldName].push(value);
+            this.data[fieldName] = [];
         });
+        this.dataLength = 0;
     }
-};
-
-Recorder.prototype.download = function() {
-    let results = this.fieldNames.join('\t') + '\n';
-    for (let i = 0; i < this.dataLength; i++) {
-        const values = this.fieldNames.map(fieldName => this.data[fieldName][i]);
-        results += values.join('\t') + '\n';
+    update() {
+        if (this.world.time % this.interval === 0) {
+            this.dataLength++;
+            this.fieldNames.forEach(fieldName => {
+                const value = this.fields[fieldName](this.world);
+                this.data[fieldName].push(value);
+            });
+        }
     }
-    download(results);
-};
-
-
-// Saves creature data into an array.
-const CreatureRecorder = function(keys) {
-    this.keys = keys;
-    this.data = [];
+    download() {
+        let results = this.fieldNames.join('\t') + '\n';
+        for (let i = 0; i < this.dataLength; i++) {
+            const values = this.fieldNames.map(fieldName => this.data[fieldName][i]);
+            results += values.join('\t') + '\n';
+        }
+        download(results);
+    }
 }
 
-CreatureRecorder.prototype.record = function(creature) {
-    this.data.push(creature);
-};
-
-CreatureRecorder.prototype.download = function() {
-    let results = '';
-    this.data.forEach(creature => {
-        results += this.keys.map(key => creature[key]).join('\t') + '\n';
-    });
-    download(results);
-};
-
+// Saves creature data into an array.
+class CreatureRecorder {
+    constructor(keys) {
+        this.keys = keys;
+        this.data = [];
+    }
+    record(creature) {
+        this.data.push(creature);
+    }
+    download() {
+        let results = '';
+        this.data.forEach(creature => {
+            results += this.keys.map(key => creature[key]).join('\t') + '\n';
+        });
+        download(results);
+    }
+}
 
 function download(data, filename="data.txt") {
     const element = document.createElement('a');
