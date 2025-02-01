602cd7ad43fb654de7b2ac4ea0184e760d23c1b1
Peter Collingridge <peter.collingridge@nanoporetech.com>
2025-02-01 12:51:42 +0000

    Add bloop 6 with spikes

---
+++ js/src/organisms/bloops/Bloop6.js
@@ -0,0 +1,35 @@
+// Bloop4 is a Bloop with a gene controlling its spikiness
+// The more spiky, the the more damage it deals when it hits another bloop.
+//  - Gene 0: spikiness
+
+// require ./Bloop.js
+
+class Bloop6 extends Bloop {
+    constructor(position, energy, genome) {
+        super(position, energy, genome);
+        this.childType = Bloop6;
+    }
+    static getRandomGenome() {
+        return [randomInRange(1, 100)];
+    }
+    calculatePhenotype() {
+        this.r = 3;
+        this.spikiness = this.genome[0];
+    }
+    getChildGenome() {
+        const childGenome = this.genome.map(gene => mutate(gene));
+        return childGenome;
+    }
+    getColour() {
+        // Colour varies from blue (not spiky) to red (most spiky)
+        // The less energy the bloop has, the darker the colour
+        const energy = Math.max(0, Math.min(1, this.energy / 500));
+        const red = energy * 255 * Math.min(1, this.spikiness / 50);
+        const blue = energy * 255 * Math.min(1, (100 - this.spikiness) / 50);
+
+        return `rgba(${red}, 0, ${blue}, 160)`;
+    }
+    _extra_info() {
+        return { Spikiness: this.genome[0] };
+    }
+}
---
+++ js/src/simulations/sim-6.js
@@ -0,0 +1,32 @@
+/**********************************************************************
+ * Simulation 1
+ * 
+ * Basic simulation withone creature type, moving at random, eating
+ * food and reproducing when they have enough energy.
+***********************************************************************/
+
+// require ../worlds/world.js
+// require ../organisms/bloops/Bloop.js
+
+function start(params = {}) {
+    // Create world object
+    params.creatureType = Bloop6;
+    const world = new World(params);
+
+    // Create simulation to run and display the world
+    const sim = new Simulation('bloop-sim', world);
+
+    // Record the number of creatures and food so they can be downloaded
+    sim.addRecorder('population', {
+        Time: world => world.time,
+        Food: world => world.food.length,
+        Creatures: world => world.creatures.length,
+    }, 100);
+
+    // Record energy on simulation toolbar
+    sim.addToToolbar('Energy', (world) => {
+        let energy = world.food.length * world.foodEnergy;
+        energy += sum(world.creatures, 'energy');
+        return Math.round(energy).toLocaleString();
+    });
+}
